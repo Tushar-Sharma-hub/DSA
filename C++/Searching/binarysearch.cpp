b100882a38a4059c0a19cfcf1275cb375906a880
Tushar-Sharma-hub <ts817479@gmail.com>
2025-08-06 10:26:31 +0530

    Update binarysearch.cpp

--- C++/Searching/binarysearch.cpp
+++ C++/Searching/binarysearch.cpp
@@ -11,7 +11,7 @@ int search(vector<int>& nums, int target) {
         int n=nums.size();
         int low=0,high=n-1;
         while(low<=high){
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]==target) return mid;
             else if(target>nums[mid]){
                 low=mid+1;
