95dcd0428f8e8c9e47f2ec261559c306d1998624
Tushar-Sharma-hub <ts817479@gmail.com>
2025-08-22 01:56:36 +0530

    Add files via upload

---
+++ C++/Maths/q7TwoKeys.cpp
@@ -0,0 +1,30 @@
+// 650. 2 Keys Keyboard
+
+// There is only one character 'A' on the screen of a notepad. You can perform one of two operations on this notepad for each step:
+
+// Copy All: You can copy all the characters present on the screen (a partial copy is not allowed).
+// Paste: You can paste the characters which are copied last time.
+// Given an integer n, return the minimum number of operations to get the character 'A' exactly n times on the screen.
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int highestfactor(int n){
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0) return n/i;
+        }
+        return 1;
+    }
+    int minSteps(int n) {
+        if(n==1) return 0;
+        if(n<=4) return n;
+        int count=0;
+        while(n>1){
+            int hf=highestfactor(n);
+            count+=(n/hf);
+            n=hf;
+        }
+        return count;
+    }
+};
---
+++ C++/Maths/q8Uglyno.cpp
@@ -0,0 +1,18 @@
+// 263. Ugly Number
+
+// An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+
+// Given an integer n, return true if n is an ugly number.
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n == 0) return false;
+        while(n%2==0) n/=2;
+        while(n%3==0) n/=3;
+        while(n%5==0) n/=5;
+        if (n == 1) return true;
+        else return false;
+    }
+};
