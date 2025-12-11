6e28b10e8044fc4d0f0016b0ada1bbcb83c1942c
Sharma4Tushar <ts817449@gmail.com>
2025-12-12 01:19:27 +0530

    update

--- CPP/16_Stack/q12_InfixEvaluation.cpp
+++ CPP/16_Stack/q12_InfixEvaluation.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int priority(char ch){
     if(ch=='*' || ch=='/') return 2;
-    return 1;
+    if(ch=='+' || ch=='-') return 1;
+    return 0; // for '('
 }
 
 int solve(char ch,int val1,int val2){
@@ -16,39 +17,52 @@ int solve(char ch,int val1,int val2){
 }
 
 int main(){
-    string str="2+6*4/8-3";
+    string str="(2+6)*(4/2)-3";
     stack<int> val;
-    stack<int> op;
+    stack<char> op;
+
     for(int i=0;i<str.length();i++){
-        if(str[i]>=48 && str[i]<=57){ //digit
-            val.push(str[i]-48);
+        
+        // Digit
+        if(str[i] >= '0' && str[i] <= '9'){
+            val.push(str[i]-'0');
         }
-        else{ //operation
-            if(op.size()==0 || priority(op.top())<priority(str[i])) op.push(str[i]);
-            else{
-                while(op.size()>0 && priority(op.top())>=priority(str[i])){
-                    char ch=op.top();
-                    op.pop();
-                    int val2=val.top();
-                    val.pop();
-                    int val1=val.top();
-                    val.pop();
-                    int ans=solve(ch,val1,val2);
-                    val.push(ans);
-                }
-                op.push(str[i]);
+
+        // Opening bracket
+        else if(str[i] == '('){
+            op.push('(');
+        }
+
+        // Closing bracket
+        else if(str[i] == ')'){
+            while(op.top() != '('){
+                char ch = op.top(); op.pop();
+                int val2 = val.top(); val.pop();
+                int val1 = val.top(); val.pop();
+                val.push( solve(ch,val1,val2) );
             }
+            op.pop(); // remove '('
+        }
+
+        // Operator + - * /
+        else {
+            while(!op.empty() && op.top()!='(' && priority(op.top()) >= priority(str[i])){
+                char ch = op.top(); op.pop();
+                int val2 = val.top(); val.pop();
+                int val1 = val.top(); val.pop();
+                val.push( solve(ch,val1,val2) );
+            }
+            op.push(str[i]);
         }
     }
-    while(op.size()>0){
-        char ch=op.top();
-        op.pop();
-        int val2=val.top();
-        val.pop();
-        int val1=val.top();
-        val.pop();
-        int ans=solve(ch,val1,val2);
-        val.push(ans);
+
+    // Solve remaining operators
+    while(!op.empty()){
+        char ch = op.top(); op.pop();
+        int val2 = val.top(); val.pop();
+        int val1 = val.top(); val.pop();
+        val.push( solve(ch,val1,val2) );
     }
-    cout<<val.top();
+
+    cout << val.top();
 }
