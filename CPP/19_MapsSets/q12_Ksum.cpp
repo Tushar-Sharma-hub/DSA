9cf07d1f2e25c41ad60f55eb809dfae5c2252db6
Tushar Sharma <ts817479@gmail.com>
2026-03-11 16:26:04 +0530

    1679

---
+++ CPP/19_MapsSets/q12_Ksum.cpp
@@ -0,0 +1,18 @@
+// 1679. Max Number of K-Sum Pairs
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int,int> m;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(m[k-nums[i]]>0){
+                m[k-nums[i]]--;
+                ans++;
+            }
+            else{
+                m[nums[i]]++;
+            }
+        }
+        return ans;
+    }
+};
