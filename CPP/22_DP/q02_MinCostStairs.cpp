523ae2f8a5f4089e1f5448fce7358fac2a2abf39
Tushar-Sharma-hub <ts817479@gmail.com>
2026-06-02 08:10:56 +0530

    up

--- CPP/22_DP/q02_MinCostStairs.cpp
+++ CPP/22_DP/q02_MinCostStairs.cpp
@@ -25,17 +25,14 @@ public:
 
 //Tabulation
 
-//Just like the previous approach, we start from the last step and try to reach the first step.
+// 
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int> dp(n,0);
-        dp[0]=cost[0];
-        dp[1]=cost[1];
         for(int i=2;i<n;i++){
-            dp[i]=min(dp[i-1],dp[i-2])+cost[i];
+            cost[i]+=min(cost[i-1],cost[i-2]);
         }
-        return min(dp[n-1],dp[n-2]);
+        return min(cost[n-1],cost[n-2]);
     }
 };
