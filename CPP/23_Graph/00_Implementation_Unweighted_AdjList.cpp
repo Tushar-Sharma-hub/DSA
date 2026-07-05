2fd08292bccb5b3361c149b7a545ed4ee3b76c2b
Tushar Sharma <ts817479@gmail.com>
2026-07-06 00:52:43 +0530

    vectorImp

--- CPP/23_Graph/00_Implementation_Unweighted_AdjList.cpp
+++ CPP/23_Graph/00_Implementation_Unweighted_AdjList.cpp
@@ -1,37 +1,42 @@
 #include<iostream>
 #include<vector>
-#include<list>
 using namespace std;
 
-vector<list<int>> graph; //list is doubly LL.
+vector<vector<int>> graph;
 int v;
-void add_edge(int src,int dest,bool bi_dir=true){
+
+void add_edge(int src, int dest, bool bi_dir = true) {
     graph[src].push_back(dest);
-    if(bi_dir){
+    if (bi_dir) {
         graph[dest].push_back(src);
     }
 }
 
-void display(){
-    for(int i=0;i<graph.size();i++){
-        cout<<i<<" -> ";
-        for(auto el:graph[i]){
-            cout<<el<<",";
+void display() {
+    for (int i = 0; i < graph.size(); i++) {
+        cout << i << " -> ";
+        for (auto el : graph[i]) {
+            cout << el << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-int main(){
-    cin>>v;
-    graph.resize(v,list<int> ());
+int main() {
+    cin >> v;
+
+    graph.resize(v);
+
     int e;
-    cin>>e;
-    while(e--){
-        int s,d;
-        cin>>s>>d;
-        add_edge(s,d);
+    cin >> e;
+
+    while (e--) {
+        int s, d;
+        cin >> s >> d;
+        add_edge(s, d);
     }
+
     display();
+
     return 0;
 }
--- CPP/23_Graph/01_Implementation_Weighted_AdjList.cpp
+++ CPP/23_Graph/01_Implementation_Weighted_AdjList.cpp
@@ -1,37 +1,42 @@
 #include<iostream>
 #include<vector>
-#include<list>
 using namespace std;
 
-vector<list<pair<int,int>>> graph;
+vector<vector<pair<int,int>>> graph;
 int v;
-void add_edge(int src,int dest,int weight,bool bi_dir=true){
-    graph[src].push_back({dest,weight});
-    if(bi_dir){
-        graph[dest].push_back({src,weight});
+
+void add_edge(int src, int dest, int weight, bool bi_dir = true) {
+    graph[src].push_back({dest, weight});
+    if (bi_dir) {
+        graph[dest].push_back({src, weight});
     }
 }
 
-void display(){
-    for(int i=0;i<graph.size();i++){
-        cout<<i<<" -> ";
-        for(auto el:graph[i]){
-            cout<<"("<<el.first<<","<<el.second<<")"<<",";
+void display() {
+    for (int i = 0; i < graph.size(); i++) {
+        cout << i << " -> ";
+        for (auto el : graph[i]) {
+            cout << "(" << el.first << "," << el.second << ") ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-int main(){
-    cin>>v;
-    graph.resize(v,list<pair<int,int>> ());
+int main() {
+    cin >> v;
+
+    graph.resize(v);
+
     int e;
-    cin>>e;
-    while(e--){
-        int s,d,weight;
-        cin>>s>>d>>weight;
-        add_edge(s,d,weight);
+    cin >> e;
+
+    while (e--) {
+        int s, d, weight;
+        cin >> s >> d >> weight;
+        add_edge(s, d, weight);
     }
+
     display();
+
     return 0;
 }
