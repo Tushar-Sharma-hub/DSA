b9bc4d51feab25597b020693a186c3db7b3d4515
Tushar Sharma <ts817479@gmail.com>
2026-08-10 17:56:15 +0530

    optimized

--- CPP/22_DP/q10_Triangle.cpp
+++ CPP/22_DP/q10_Triangle.cpp
@@ -4,6 +4,8 @@
 // if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
 
 //Just store the last row of the triangle in a 1D array and update it in place for each row above it.
+//Time complexity: O(n^2), where n is the number of rows in the triangle.
+//Space complexity: O(n), where n is the number of rows in the triangle.
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
@@ -15,5 +17,21 @@ public:
             }
         }
         return dp[0];
+    }
+};
+
+//T.C. O(n^2) time and O(1) space solution
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].size(); j++) {
+                int below = triangle[i + 1][j];
+                int belowRight = triangle[i + 1][j + 1];
+                triangle[i][j] += min(below, belowRight);
+
+            }
+        }
+        return triangle[0][0];
     }
 };
