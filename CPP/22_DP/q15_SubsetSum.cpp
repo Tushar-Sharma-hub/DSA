e11b0048d236b13815f9a4972c585abf51750a00
Tushar Sharma <ts817479@gmail.com>
2026-08-14 02:12:32 +0530

    lc

--- CPP/22_DP/q15_SubsetSum.cpp
+++ CPP/22_DP/q15_SubsetSum.cpp
@@ -1,6 +1,9 @@
 // Subset Sum Problem
 // Given an array of positive integers arr[] and a value sum, determine if there is a subset of arr[] with sum equal to given sum.
 
+// Recursion + Memoization
+// T.C: O(n*sum) where n=arr.size()
+// S.C: O(n*sum) + O(n) ~ O(n*sum)
 class Solution {
   public:
     bool f(int idx,vector<int>& arr,int sum,vector<vector<int>>& dp){
@@ -16,5 +19,34 @@ class Solution {
         int n=arr.size();
         vector<vector<int>> dp(n,vector<int>(sum+1,-1));
         return f(n-1,arr,sum,dp);
+    }
+};
+
+// Tabulation
+// T.C: O(n*sum) where n=arr.size()
+// S.C: O(n*sum)
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<bool>> dp(n, vector<bool>(sum + 1, false));
+        for(int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        if(arr[0] <= sum) dp[0][arr[0]] = true;
+        for(int idx = 1; idx < n; idx++) {
+            for(int target = 1; target <= sum; target++) {
+                // Not take
+                bool notTake = dp[idx - 1][target];
+                // Take
+                bool take = false;
+                if(arr[idx] <= target)
+                    take = dp[idx - 1][target - arr[idx]];
+
+                dp[idx][target] = take || notTake;
+            }
+        }
+
+        return dp[n - 1][sum];
     }
 };
