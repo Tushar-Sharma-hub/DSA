7506a27c15cd755ad88f3cc2a24fed3cffc0e540
Tushar Sharma <ts817479@gmail.com>
2026-08-16 00:41:04 +0530

    lc

--- CPP/22_DP/q15_SubsetSum.cpp
+++ CPP/22_DP/q15_SubsetSum.cpp
@@ -13,7 +13,7 @@ class Solution {
         bool notTake=f(idx-1,arr,sum,dp);
         bool take=false;
         if(arr[idx]<=sum) take=f(idx-1,arr,sum-arr[idx],dp);
-        return dp[idx][sum]=take | notTake;
+        return dp[idx][sum]=take || notTake;
     }
     bool isSubsetSum(vector<int>& arr, int sum) {
         int n=arr.size();
@@ -31,9 +31,9 @@ class Solution {
         int n = arr.size();
         vector<vector<bool>> dp(n, vector<bool>(sum + 1, false));
         for(int i = 0; i < n; i++) {
-            dp[i][0] = true;
+            dp[i][0] = true; //because sum=0 can be achieved by taking no elements
         }
-        if(arr[0] <= sum) dp[0][arr[0]] = true;
+        if(arr[0] <= sum) dp[0][arr[0]] = true; //because sum=arr[0] can be achieved by taking the first element
         for(int idx = 1; idx < n; idx++) {
             for(int target = 1; target <= sum; target++) {
                 // Not take
