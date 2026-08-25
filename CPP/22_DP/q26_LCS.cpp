94e259bb135c5871a3866d03292d0db18896850d
Tushar Sharma <ts817479@gmail.com>
2026-08-26 00:39:13 +0530

    so

--- CPP/22_DP/q26_LCS.cpp
+++ CPP/22_DP/q26_LCS.cpp
@@ -44,3 +44,26 @@ public:
         return dp[n1][n2];
     }
 };
+
+//Space optimization.
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1=text1.size();
+        int n2=text2.size();
+        if (n1 < n2) return longestCommonSubsequence(text2, text1);
+        vector<int> prev(n2+1,0),curr(n2+1,0);
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(text1[i-1]==text2[j-1]){
+                    curr[j]=1+prev[j-1];
+                }
+                else{
+                    curr[j]=max(prev[j],curr[j-1]);
+                }
+            }
+            prev=curr;
+        }
+        return prev[n2];
+    }
+};
