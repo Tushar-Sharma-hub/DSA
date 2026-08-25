a77b2a9ac57da0cb903bd6b2370a3b3c69ed4f2b
Tushar Sharma <ts817479@gmail.com>
2026-08-26 01:54:07 +0530

    printLCS

--- CPP/22_DP/q26_LCS.cpp
+++ CPP/22_DP/q26_LCS.cpp
@@ -67,3 +67,49 @@ public:
         return prev[n2];
     }
 };
+
+//To print LCS.
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.size();
+        int n2 = text2.size();
+
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+
+        for(int i = 1; i <= n1; i++){
+            for(int j = 1; j <= n2; j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        string lcs = "";
+        int i = n1;
+        int j = n2;
+
+        while(i > 0 && j > 0){
+            if(text1[i-1] == text2[j-1]){
+                lcs += text1[i-1];
+                i--;
+                j--;
+            }
+            else if(dp[i-1][j] > dp[i][j-1]){
+                i--;
+            }
+            else{
+                j--;
+            }
+        }
+
+        reverse(lcs.begin(), lcs.end());
+
+        cout << "LCS: " << lcs << endl;
+
+        return dp[n1][n2];
+    }
+};
