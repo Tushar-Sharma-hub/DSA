2b664f132d5dab4cb7579609d8ab3f45f3a46643
Tushar Sharma <ts817479@gmail.com>
2026-08-29 03:29:13 +0530

    tab

--- CPP/22_DP/04_DP_Strings/q32_DistSubseq.cpp
+++ CPP/22_DP/04_DP_Strings/q32_DistSubseq.cpp
@@ -10,18 +10,43 @@
 class Solution {
 public:
     int f(vector<vector<int>>& dp,int idx1,int idx2,string& s,string & t){
-        if(idx2>=t.size()) return 1;
-        if(idx1>=s.size()) return 0;
+        if(idx2<0) return 1;
+        if(idx1<0) return 0;
         if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
         if(s[idx1]==t[idx2]){
-            int take = f(dp,idx1+1, idx2+1, s, t);
-            int notTake = f(dp,idx1+1, idx2, s, t);
+            int take = f(dp,idx1-1, idx2-1, s, t);
+            int notTake = f(dp,idx1-1, idx2, s, t);
             return dp[idx1][idx2]=take + notTake;
         }
-        return dp[idx1][idx2]=f(dp,idx1+1,idx2,s,t);
+        return dp[idx1][idx2]=f(dp,idx1-1,idx2,s,t);
     }
     int numDistinct(string s, string t) {
         vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
-        return f(dp,0,0,s,t);
+        return f(dp,s.size()-1,t.size()-1,s,t);
+    }
+};
+
+//Tabulation
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<unsigned long long>> dp(n + 1,vector<unsigned long long>(m + 1, 0));
+        // Empty t can be formed in exactly one way
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                // Don't take s[i-1]
+                dp[i][j] = dp[i-1][j];
+                // Take s[i-1]
+                if(s[i-1] == t[j-1]) {
+                    dp[i][j] += dp[i-1][j-1];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
