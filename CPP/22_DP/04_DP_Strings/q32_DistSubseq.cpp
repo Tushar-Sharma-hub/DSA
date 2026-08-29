00a35a235b5be6ed2582f587bc9d23fdbe4d7b87
Tushar Sharma <ts817479@gmail.com>
2026-08-29 17:16:01 +0530

    so

--- CPP/22_DP/04_DP_Strings/q32_DistSubseq.cpp
+++ CPP/22_DP/04_DP_Strings/q32_DistSubseq.cpp
@@ -50,3 +50,24 @@ public:
         return dp[n][m];
     }
 };
+
+//Space optimization
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n1=s.size();
+        int n2=t.size();
+        vector<double> prev(n2+1,0),curr(n2+1,0);
+        prev[0]=curr[0]=1;
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                curr[j]=prev[j];
+                if(s[i-1]==t[j-1]){
+                    curr[j]+=prev[j-1];
+                }
+            }
+            prev=curr;
+        }
+        return prev[n2];
+    }
+};
