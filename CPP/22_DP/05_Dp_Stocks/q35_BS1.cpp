4dc779a3e0cae7d1b2df36ad06959f72234ffef9
Tushar Sharma <ts817479@gmail.com>
2026-09-04 16:35:55 +0530

    121

---
+++ CPP/22_DP/05_Dp_Stocks/q35_BS1.cpp
@@ -0,0 +1,21 @@
+// 121. Best Time to Buy and Sell Stock
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0
+
+//Just one pass through the array, keep track of the minimum price seen so far 
+//and the maximum profit that can be achieved by selling on the current day.
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy=prices[0];
+        int ans=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]>buy){
+                ans=max(ans,prices[i]-buy);
+            }
+            buy=min(buy,prices[i]);
+        }
+        return ans;
+    }
+};
